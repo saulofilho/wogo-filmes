2237961bf01455ae7df90f868a0bd5b75061f318
saulo filho <hello@saulofilho.com>
2021-10-14 11:51:42 -0300

    [fix] contato callback

--- src/pages/contato.js
+++ src/pages/contato.js
@@ -2,6 +2,8 @@ import React from 'react'
 import Layout from "../components/Layout"
 import './Contato.scss'
 
+import Logo from '../../static/assets/logos/logo.jpg'
+
 const Contato = () => {
   return (
     <Layout>
@@ -11,7 +13,10 @@ const Contato = () => {
           Vamos conversar?
         </p>
         <div class="form-row">
-          <form method="post" netlify-honeypot="bot-field" data-netlify="true" name="contact">
+          <div className="contato-text">
+            <img src={Logo} alt="wogo filmes logo" />
+          </div>
+          <form method="post" netlify-honeypot="bot-field" data-netlify="true" name="contact" action="/clientes">
             <input type="hidden" name="bot-field" />
             <input type="hidden" name="form-name" value="contact" />
             <label>Seu nome: <input type="text" name="name" placeholder="Seu Nome" /></label>
@@ -20,20 +25,6 @@ const Contato = () => {
               placeholder="O seu texto aqui..."></textarea></label>
             <button type="submit">Send</button>
           </form>
-          <div className="contato-text">
-            <p>
-              Email: contato@wogofilmes.com.br
-              <br />
-              <br />
-              Vimeo: vimeo.com/wogofilmes
-              <br />
-              <br />
-              Instagram: instagram.com/wogofilmes
-              <br />
-              <br />
-              Endereço: Avenida Rio Branco, Nº 404, Sala 307, Bloco 1 - Centro, Florianópolis - SC
-            </p>
-          </div>
         </div>
       </div>
     </Layout>
