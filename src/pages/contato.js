1626a14192af09c66362bbac8b4a2bdf4aab4175
Saulo Filho <32524964+saulofilho@users.noreply.github.com>
2021-10-26 13:30:31 -0400

    Update contato.js

--- src/pages/contato.js
+++ src/pages/contato.js
@@ -2,8 +2,6 @@ import React from 'react'
 import Layout from "../components/Layout"
 import './Contato.scss'
 
-// import Logo from '../../static/assets/logos/logo.jpg'
-
 const Contato = () => {
   return (
     <Layout>
@@ -13,15 +11,12 @@ const Contato = () => {
           Vamos conversar?
         </p>
         <div class="form-row">
-          {/* <div className="contato-text">
-            <img src={Logo} alt="wogo filmes logo" />
-          </div> */}
           <form method="post" netlify-honeypot="bot-field" data-netlify="true" name="contact" action="/contato-ok">
             <input type="hidden" name="bot-field" />
             <input type="hidden" name="form-name" value="contact" />
             <label>Seu nome: <input type="text" name="name" placeholder="Seu Nome" /></label>
             <label>Seu email: <input type="email" name="email" placeholder="seuemail@gmail.com" /></label>
-            <label>Menssagem: <textarea name="message"
+            <label>Mensagem: <textarea name="message"
               placeholder="O seu texto aqui..."></textarea></label>
             <button type="submit">Send</button>
           </form>
